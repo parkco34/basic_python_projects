ed8acd4ce3e49b12234521e5db9d6dcfc04eb440
parkco34 <parkercorya@gmail.com>
2022-07-17 15:37:45 -0400

    Simple OOP Game

---
+++ simple_oop_game/README.md
@@ -0,0 +1,145 @@
+# MyGame
+> The goal of this game is to escape the BASEMENT by improving your mental health in a such a way, you become strong enough to escape before the clock runs out.  Otherwise you're DEAD.
+
+## SYNOPSIS:
+
+"   You're trapped in a BASEMENT that's flooding with no doors.  Only a WINDOW, with which you may
+escape.  You're too weak and stupid to open it right now, though...
+
+It's a Basement that is composed
+of heirarchies representing your MENTAL STATE.
+
+   Beginning at the ABYSS level, with minimum HEALTH made up of pHYSICAL,
+    MENTAL, and HENRI.  The first enemy you encounter, the Re-Sister, must be
+    fought in order to go on to the next MIND STATE, and so on until you
+    eventually reach the WINDOW...  There are THREE main BOSSES in which block
+    you from escaping the Basement in a timely manner, and a problem to solve
+    prior to freeing yourself through the Window. Between the Abyss and the
+    Window, you must enter Dreams, Awake, and Revenge, evolving into CONAN, the
+    only person strong enough physically and mentally to open the window."
+
+___
+
+*Death*: Player dies, with some "last words"
+
+*Abyss*: Starting point of game, where you have to get beyond "level 1" before
+continuing on to different MIND SETS by either READING, EXERCISING or Playing with Henri.  This is where the FIRST "DECISION" (what you decide to do to level up/down and why) is likely to KILL you or LEVEL YOU UP more than the subsequent MentalStates.
+
+*Dreams*: MentalState where player is in a "dream state," battling with
+the Leiche_ML Boss, via READING, EXERCISING, PLAYING WITH HENRI
+
+*Awake*: Awaking from the "dream state", only to battle a stronger enemy, where
+you will have to BALANCE your health components just right (might mean DRINKING
+        VODKA, or something) in order to get to the WINDOW MentalState.
+
+*Revenge*: Mental state that requires battling the MIrror after you find the SWORD and/or GENIUS, without which, you could not open the Window.
+
+*WINDOW*: This is where the HERO (Player) escapes the BASEMENT, but only after
+successfully solving a PUZZLE.
+
+___
+
+## EXTRACT KEY CONCEPTS and RESEARCH:
+(NOUNS)
+
+- Hero
+- Leiche_ML (Mid-Level Boss)
+- Basement (world)
+- MentalState
+- Abyss
+- Dreams
+- Window
+- Re-Sister (Beginner Boss)
+- Mirror (Final Boss)
+- Death
+- Engine
+- Map 
+- Sword
+- Genius
+- Puzzle
+
+## Class Heirarchy and Object Map for Concepts:
+(*What's similar to other things?*)
+
+* Map
+* Engine
+* MentalState
+    * Death
+    * Abyss
+    * Dreams
+    * Awake
+    * Window
+
+(*VERBS* included):
+
+* Map
+    - next_mental_state 
+    - entering_mental_state
+
+* Engine
+    - play
+
+* MentalState
+    - enter
+    * Death
+    * Abyss
+    * Dreams
+    * Awake
+    * Revenge
+    * Window
+
+## Pseudocode: (Skeletal and TOP-DOWN Approach)
+
+class MentalState
+    
+    def enter
+
+
+class Engine
+
+    constructor(MAP_OBJECT)
+    
+    def play
+
+
+class Death(MentalState)
+
+    def enter
+
+
+class Abyss(MentalState)
+
+    def enter
+
+
+class Dreams(MentalState)
+
+    def enter
+
+
+class Revenge(MentalState)
+
+    def enter
+
+
+class Window(MentalState)
+
+    def enter
+
+
+class Map
+
+    constructor(MentalState_Object)
+
+    def next_mental_state
+
+    def open_mental_state
+
+
+## Repeatand Refine:
+
+
+
+
+
+
---
+++ simple_oop_game/simpl_game.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python
+from textwrap import dedent
+from random import randint
+from sys import exit
+
+
+class MentalState(object):
+
+    def enter(self):
+        print("Mental States not configured yet!")
+        exit(1)
+
+
+class Death(MentalState):
+
+    quips = [
+
+    """Every adversity, every failure, every heartache carries with it the
+    seed of an equal or greater benefit.
+    """,
+    """
+    What is the point of being alive if you don't at least try to do something remarkable?
+    """,
+    """
+    That which does not kill us makes us stronger
+    """,
+    """
+    A thinker sees his own actions as experiments and questions--as attempts to find out something. Success and failure are for him answers above all.
+    """,
+    """
+    There are few pains so grievous as to have seen, divined, or experienced how an exceptional man has missed his way and deteriorated
+    """,
+    """
+    Many a man fails to be a thinker simply because his memory is too good.
+    """,
+    """
+    Success is not final, failure is not fatal: it is the courage to continue that counts.
+    """,
+    """
+    Success is stumbling from failure to failure with no loss of enthusiasm.
+    """,
+    """
+    My fault, my failure, is not in the passions I have, but in my lack of control of them.
+    """,
+    """
+    All of old. Nothing else ever. Ever tried. Ever failed. No matter. Try again. Fail again. Fail better.
+    """,
+    """
+    It is hard to fail, but it is worse never to have tried to succeed.
+    """,
+    """
+    All men make mistakes, but a good man yields when he knows his course is wrong, and repairs the evil. The only crime is pride.
+    """
+    ]
+
+    def enter(self):
+        print(Death.quips[randint(0, len(self.quips)-1)])
+        exit(1)
+
+
+
+class Abyss(MentalState):
+
+    def enter(self):
+        print(dedent("""
+         You regain consciousness only to find yourself in a cold,
+         damp basement.  After looking for a way out, you realize
+         the only way to escape is through the WINDOW...
+         Unfortunately, you're not strong enough to pull it open!
+         What's worse, even if you could open it, there's some kind
+         of weird puzzle you have to solve before it can open
+         completely.
+
+         As if you weren't stressed enough, you see a puddle of water in the
+                     corner.  It's MOVING!  The BASEMENT IS FLOODING!!! No time
+                     to waste!  We have to figure out how to get out...
+
+        (Heavy breathing behind you) A Re-Sister is attacking you!!!
+                     \n"""))
+
+        action = input("> ").lower()
+
+        if action == "exercise":
+            print(dedent("""
+                         \nYou SWOLE UP, capable of lifting a car.  You
+                         uppercut the beast in one of its four armpits and
+                         round house kick the fool in the right temple
+                         incompasiating it... You decapitate the S.O.B\n
+                         """))
+            return 'dreams'
+
+        elif action == "read":
+            print(dedent("""
+                         \nYou pick up an book on Abstract Algebra and attempt
+                         to read it, but the Re-Sister looks on judgmentally
+                         and then devours your head clean off your
+                         shoulders!\n
+                         """))
+            return 'death'
+
+        elif action == 'henri':
+            print(dedent("""
+                         \nYou start playing with Henri using a bird toy... The
+                         Creature takes the birdy away from you in jealousy and
+                         begins playing with the Bengal... it thinks it has a
+                         friend.  CUTE...
+                         """))
+            return 'superwin'
+
+        elif action == "drink" or "gummies":
+            print(dedent("""
+                         \nYou turn off the lights and crack open a pint of
+                         Smirnoff... Time for a stimulating movie!
+                         """))
+            return 'abyss'
+
+        else:
+            print("DOESN'T COMPUTE!")
+            return 'abyss'
+
+
+class Dreams(MentalState):
+
+    def enter(self):
+        print(dedent("""
+                     \nProud with your little win, you fall deep asleep...
+                     Falling into an unimaginable HELL some people call,
+                     Dreaming, where a monster appears charging you!  It's a
+                     damn Lieche_ML!!!! NOOOOOOOO!!!!!\nWhat's this??  She
+                     challenges you to a game of WITS!!\n
+                     """))
+        
+        code = f"{randint(1,9)}{randint(1,9)}{randint(1,9)}"
+        guess = input("[keypad]> ")
+        guesses = 0
+
+        while guess != code and guesses < 10:
+            print("BEEP BEEP BEEP")
+            guesses += 1
+            guess = input("[keypad]> ")
+
+
+class Awake(MentalState):
+
+    def enter(self):
+        pass
+
+
+class Reveng(MentalState):
+
+    def enter(self):
+        pass
+
+
+class Window(MentalState):
+
+    def enter(self):
+        pass
+
+
+
+class Engine(object):
+    
+    def __init__(self, mental_map):
+        self.mental_map = mental_map
+
+    def play(self):
+        current_mental_state = self.mental_map.open_mental_state()
+        last_mental_state = self.mental_map.next_mental_state('finished')
+
+        while current_mental_state != last_mental_state:
+            next_state_name = current_mental_state.enter()
+            current_mental_state =\
+            self.mental_map.next_mental_state(next_state_name)
+
+        current_mental_state.enter()
+
+
+class Map(object):
+
+    def __init__(self, initial_mental_state):
+        self.initial_mental_state = initial_mental_state
+
+    def next_mental_state(self):
+        pass
+
+    def open_mental_state(self, something):
+        pass
+
+
+
+
+
